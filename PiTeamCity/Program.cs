83970ae3b41bd61f2697a2502c08f29f8f5539e7
Quant1co <115133527+Quant1co@users.noreply.github.com>
2023-12-14 19:22:15 +0300

    create new project

---
+++ PiTeamCity/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIteamcity
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            CheckFermatTheorem(1);
+        }
+        static void CheckFermatTheorem(int n)
+        {
+            for (int a = 1; a < n; a++)
+            {
+                for (int b = 1; b < n; b++)
+                {
+                    for (int c = 1; c < n; c++)
+                    {
+                        if (Math.Pow(a, n) + Math.Pow(b, n) == Math.Pow(c, n))
+                        {
+                            Console.WriteLine("Fermat's theorem is wrong!");
+                            return;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("Fermat's theorem holds for all numbers up to " + n);
+        }
+    }
+}
+
+//измение в коде
